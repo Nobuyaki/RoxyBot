69ff5e9bf5779ec82ae15fd97b020de0a5776795
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-07-01 23:01:19 +0700

    events loader

---
+++ events/ready.js
@@ -0,0 +1,18 @@
+const { prefix } = require("../data/config.json");
+
+module.exports = (client) => {
+      console.log(`Hi, ${client.user.username} is now online!`);
+      let presences = [
+      { type: "LISTENING", activity: `${prefix}help` },
+      { type: "PLAYING", activity: `With My Owner` },
+      ];
+      client.user.setActivity(presences[0].activity, { type: presences[0].type });
+      var index = 1;
+      if (!presences.length) return;
+      if (presences.length == 1) return;
+      setInterval(() => {
+      if (index > presences.length - 1) index = 0;
+      client.user.setActivity(presences[index].activity, { type: presences[index].type });
+      index++;
+      }, 5000);
+}
