e8cea54c2ed565962afc978072414d34a2d3beae
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-07-02 03:47:23 +0700

    Update avatar.js

--- commands/info/avatar.js
+++ commands/info/avatar.js
@@ -8,13 +8,17 @@ module.exports = {
     tags: "info",
 
     run: async (client, message, args) => {
-        let mention = message.mentions.users.first() || message.author;
+        let member = message.mentions.members.first() || message.member || messsage.guild.members.cache.get(args[0]), 
+            user  = member.user;
+        if (!member) {
+            throw 'Member Tidak Di Temukan!';
+        }
 
         let embed = new Discord.MessageEmbed().setColor('#7f5ce7')
-        .setImage(mention.displayAvatarURL({ size: 4096, format: 'png', dynamic: true }))
+        .setImage(member.displayAvatarURL({ size: 4096, format: 'png', dynamic: true }))
         .setTimestamp()
-        .setDescription(`[\`link avatar\`](${mention.displayAvatarURL({ size: 4096, format: 'png', dynamic: true })})`)
-        .setFooter(`Avatar of ${mention.tag}`)
+        .setDescription(`[\`link avatar\`](${member.displayAvatarURL({ size: 4096, format: 'png', dynamic: true })})`)
+        .setFooter(`Avatar ${member.tag}`)
         message.channel.send(embed)
     }
 }
