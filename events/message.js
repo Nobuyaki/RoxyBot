cde183ac67dd3c38decd59cbe9c3bc44510b351c
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-07-01 23:03:40 +0700

    events loader

---
+++ events/message.js
@@ -0,0 +1,19 @@
+const { prefix } = require("../data/config.json")
+
+module.exports = async (client, message) => {
+    if (message.author.bot) return;
+    if (!message.guild) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    if (cmd.length === 0) return;
+
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+
+    if (command)
+        command.run(client, message, args);
+}
