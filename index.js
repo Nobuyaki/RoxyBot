80179b33a81497db75dbf7aadb7b9e4a89572db0
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-07-02 04:56:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const { config } = require('dotenv'):
 const fs = require('fs');
 
-const { token, prefix } = require("./data/config.json");
+const data = require("./data/config.js");
 
 const client = new Discord.Client({
      disableEveryone: true
@@ -10,6 +10,9 @@ const client = new Discord.Client({
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
+client.data = require('./data/config.js');
+client.emotes = client.data.emotes;
+client.colors = client.data.colors;
 
 client.categories = fs.readdirSync("./commands/");
 
