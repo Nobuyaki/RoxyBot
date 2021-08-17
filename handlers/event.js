8c4955fc2bef481f28ec7f73844a7fde53b60c14
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-08-18 00:45:50 +0700

    Update event.js

--- handlers/event.js
+++ handlers/event.js
@@ -1,7 +1,7 @@
 module.exports = (client) => {
-    const events = fs.readdirSync("../events/");
-      for (let event of events) {
-    let file = require(`../events/${event}`);
-    client.on(event.split(".")[0], (...args) => file(client, ...args));
-};
- }
+	const events = fs.readdirSync("./events/");
+		for (let event of events) {
+			let file = require(`../events/${event}`);
+   			client.on(event.split(".")[0], (...args) => file(client, ...args));
+		};
+}
