ccf893d8862af77dfd126c824c6da68d369940a8
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-08-22 11:19:05 +0700

    Create play.js

---
+++ commands/music/play.js
@@ -0,0 +1,6 @@
+
+const ytdl = require("discord-ytdl-core");
+const youtubeScraper = require("yt-search");
+const yt = require("ytdl-core");
+const { MessageEmbed, Util } = require("discord.js");
+const forHumans = require("../utils/forhumans.js");
