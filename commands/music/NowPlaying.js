a6a23898c2e3938b83d8a0fd9f764921dbf8b10a
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-08-22 11:55:13 +0700

    Create NowPlaying.js

---
+++ commands/music/NowPlaying.js
@@ -0,0 +1,41 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "nowplaying",
+    description: "Menampilkan Yang Sedang di Putar",
+    aliases: [ "np" ],
+    usage: `nowplaying`,
+    tags: "music",
+
+    run: async (client, message) => {
+  const channel = message.member.voice.channel;
+  if (!channel)
+    return message.channel.send(
+      "You must Join a voice channel before using this command!"
+    );
+  let queue = message.client.queue.get(message.guild.id);
+  if (!queue)
+    return message.channel.send(
+      new MessageEmbed()
+        .setColor("RED")
+        .setDescription(":x: There are no songs playing in this server")
+    );
+  message.channel.send(
+    new MessageEmbed()
+      .setAuthor(
+        "Now Playing",
+        "https://img.icons8.com/color/2x/audio-wave--v2.gif"
+      )
+      .setColor("BLUE")
+      .setDescription(
+        queue.queue[0].name +
+          " Requested By: " +
+          "<@" +
+          queue.queue[0].requested +
+          ">"
+      )
+      .setThumbnail(queue.queue[0].thumbnail)
+      .setFooter("There are " + queue.queue.length + " songs in queue")
+  );
+ }
+};
