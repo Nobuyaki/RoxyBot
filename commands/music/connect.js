e801007a0449a3b8d3ef70b3244ec26db393eb11
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-08-22 11:29:06 +0700

    Create connect.js

---
+++ commands/music/connect.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "connect",
+    description: "Memasukan bot ke voice",
+    aliases: [ "join" ],
+    usage: `connect`,
+    tags: "music",
+
+    run: async (client, message, args) => {
+
+  const channel = message.member.voice.channel;
+  if (!channel)
+    return message.channel.send(
+      "You must Join a voice channel before using this command!"
+    );
+
+  if (!channel.permissionsFor(message.client.user).has("CONNECT"))
+    return error("I don't have permission to join the voice channel");
+
+  if (!channel.permissionsFor(message.client.user).has("SPEAK"))
+    return error("I don't have permission to speak in the voice channel");
+
+  await channel.join();
+
+  return message.channel.send(
+    new MessageEmbed()
+      .setDescription("**Joined the voice channel :white_check_mark: **")
+      .setColor("BLUE")
+  );
+ } 
+};
