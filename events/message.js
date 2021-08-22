7d3b73a376f52f250f3d24ec39ad34296b13e305
nobuyaki <72033395+Nobuyaki@users.noreply.github.com>
2021-08-22 13:35:39 +0700

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,15 +1,12 @@
 const { prefix } = require("../data/config.json")
-const jsoning = require("jsoning");
-const database = new jsoning("../data/database.json");
 
 module.exports = async (client, message) => {
-    const Gprefix = database.get() || prefix;
     if (message.author.bot) return;
     if (!message.guild) return;
-    if (!message.content.startsWith(Gprefix)) return;
+    if (!message.content.startsWith(prefix)) return;
     if (!message.member) message.member = await message.guild.fetchMember(message);
 
-    const args = message.content.slice(Gprefix.length).trim().split(/ +/g);
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
 
     if (cmd.length === 0) return;
